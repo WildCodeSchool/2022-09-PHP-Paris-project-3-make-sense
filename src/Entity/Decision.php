2e17babd92691d1a0408c158fd86c18fb25a9f19
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2022-12-07 17:16:38 +0100

    create table decision

---
+++ src/Entity/Decision.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DecisionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DecisionRepository::class)]
+class Decision
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $impact = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $benefits = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $risks = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImpact(): ?string
+    {
+        return $this->impact;
+    }
+
+    public function setImpact(?string $impact): self
+    {
+        $this->impact = $impact;
+
+        return $this;
+    }
+
+    public function getBenefits(): ?string
+    {
+        return $this->benefits;
+    }
+
+    public function setBenefits(string $benefits): self
+    {
+        $this->benefits = $benefits;
+
+        return $this;
+    }
+
+    public function getRisks(): ?string
+    {
+        return $this->risks;
+    }
+
+    public function setRisks(?string $risks): self
+    {
+        $this->risks = $risks;
+
+        return $this;
+    }
+}
