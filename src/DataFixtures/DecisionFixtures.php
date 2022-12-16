49fce9a1f032c896fac86e83d1b37980e1376131
jeremy <jeremy.guimard.pro@gmail.com>
2022-12-16 10:15:09 +0100

    Decision fixtures version 0.1

---
+++ src/DataFixtures/DecisionFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Entity\Decision;
+use Faker;
+
+class DecisionFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const USER = 5;
+    public const DECISION = 5;
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < self::USER; $i++ )
+        {
+            for($j = 0; $j < self::DECISION; $j++ )
+            {
+            $decision = new Decision();
+            $decision = $faker->text(25);
+            $decision->setTitle($faker->word(25, true));
+            $decision->setDescription($faker->text(25));
+            $decision->setImpacts($faker->text(25));
+            $decision->setBenefits($faker->text(25));
+            $decision->setRisks($faker->text(25));
+            $decision->setLikeThreshold($faker->numberBetween(30, 70));
+            $decision->setCreatedAt(new \DateTime('now'));
+            $decision->setUpdatedAt(new \DateTime('now'));
+            $decision->setCreatedBy($this->getReference('created_by_'.rand(1,UserFixtures::NB_USER)));
+            $this->addReference('decision_'.$j, $decision);
+            $manager->persist($decision);
+            }
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+
+    {
+        return [UserFixtures::class];
+    }
+}
