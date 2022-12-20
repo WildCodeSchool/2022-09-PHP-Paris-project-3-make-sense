bc083ed5601eccd46c109fefb3348046228155c1
Cedric Nkene <cedric.nkn@gmail.com>
2022-12-20 16:09:34 +0100

    decision_user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\DecisionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/user', name: 'user_')]
+class UserController extends AbstractController
+{
+    #[Route('/{user}', methods: ['GET'], name: 'show')]
+    public function show(User $user, DecisionRepository $decisionRepository): Response
+    {
+
+        // dd($user);
+        return $this->render('user/show.html.twig', ['user'=>$user, 'decisions' => $decisionRepository->findBy(
+            ['owner' => $user],
+            ['createdAt' => 'DESC'])]);
+    }
+}
