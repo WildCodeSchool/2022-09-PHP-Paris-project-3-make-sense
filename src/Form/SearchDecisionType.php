bf0e22a3560f9b57abad62a1bee8889ef5302511
jeremy <jeremy.guimard.pro@gmail.com>
2023-01-03 15:06:14 +0100

    save work and create sql request

--- src/Form/SearchDecisionType.php
+++ src/Form/SearchDecisionType.php
@@ -9,6 +9,7 @@ use App\Repository\HistoryRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 
@@ -24,6 +25,22 @@ class SearchDecisionType extends AbstractType
                 'label' => 'Chercher une décision',
                 'label_attr' => [
                     'class' => 'form-label mt-5'
+                ],
+            ])
+            ->add('domaines', EntityType::class, [
+                'class'    => Department::class,
+                'expanded' => true,
+                'multiple' => true,
+            ])
+            ->add('Status', ChoiceType::class, [
+                'choices' => [
+                   
+                 History::STATUS[0] => History::STATUS[0],
+                 History::STATUS[1] => History::STATUS[1],
+                 History::STATUS[2] => History::STATUS[2],
+                 History::STATUS[3] => History::STATUS[3],
+                 History::STATUS[4] => History::STATUS[4],
+                 History::STATUS[5] => History::STATUS[5],
                 ],
             ]);
     }
