99ba7555473e5ad6551793f066b47682c213fb65
Cedric Nkene <cedric.nkn@gmail.com>
2023-01-06 10:27:12 +0100

    user_decision

--- assets/app.js
+++ assets/app.js
@@ -15,3 +15,5 @@ import './bootstrap';
 // // this "modifies" the jquery module: adding behavior to it
 // // the bootstrap module doesn't export/return anything
 // require('bootstrap');
+
+import "@fontsource/raleway";
