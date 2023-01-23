a69e387a0c0a4505be39f48b763edce9d5fc6cf8
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2023-01-23 12:45:53 +0100

    update review mickael

--- src/Entity/History.php
+++ src/Entity/History.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\HistoryRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -38,14 +36,6 @@ class History
 
     #[ORM\Column]
     private ?DateTimeImmutable $createdAt = null;
-
-    // #[ORM\OneToMany(mappedBy: 'history', targetEntity: Notification::class)]
-    // private Collection $notifications;
-
-    public function __construct()
-    {
-        // $this->notifications = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
