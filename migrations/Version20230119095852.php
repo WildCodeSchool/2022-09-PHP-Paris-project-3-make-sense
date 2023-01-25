4c282cf4c367a3a2bb6858531d5e94000d83bff5
neha75010 <73582439+neha75010@users.noreply.github.com>
2023-01-25 14:31:19 +0100

    Adding a background image and adjusting the form to the login page

--- migrations/Version20230119095852.php
+++ migrations/Version20230119095852.php
@@ -20,9 +20,6 @@ final class Version20230119095852 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE decision DROP FOREIGN KEY FK_84ACBE48EF1A9D84');
-        $this->addSql('DROP INDEX IDX_84ACBE48EF1A9D84 ON decision');
-        $this->addSql('ALTER TABLE decision DROP notification_id');
         $this->addSql('ALTER TABLE notification ADD decision_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE notification ADD CONSTRAINT FK_BF5476CABDEE7539 FOREIGN KEY (decision_id) REFERENCES decision (id)');
         $this->addSql('CREATE INDEX IDX_BF5476CABDEE7539 ON notification (decision_id)');
@@ -34,8 +31,5 @@ final class Version20230119095852 extends AbstractMigration
         $this->addSql('ALTER TABLE notification DROP FOREIGN KEY FK_BF5476CABDEE7539');
         $this->addSql('DROP INDEX IDX_BF5476CABDEE7539 ON notification');
         $this->addSql('ALTER TABLE notification DROP decision_id');
-        $this->addSql('ALTER TABLE decision ADD notification_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE decision ADD CONSTRAINT FK_84ACBE48EF1A9D84 FOREIGN KEY (notification_id) REFERENCES notification (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('CREATE INDEX IDX_84ACBE48EF1A9D84 ON decision (notification_id)');
     }
 }
