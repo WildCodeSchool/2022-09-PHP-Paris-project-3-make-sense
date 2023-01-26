a77fc0657f56aaed5e50c2ae69201b708236ac43
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2023-01-26 10:08:19 +0100

    cron

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    public const USERID = 51;
+    public const USERID = 51 ;
 
     #[Route('/', name: 'app_home')]
     public function index(
