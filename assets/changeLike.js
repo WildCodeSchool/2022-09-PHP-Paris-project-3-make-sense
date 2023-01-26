985695453ba18bad98b28a0e7e8d2c928e2acf5f
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2023-01-26 11:34:30 +0100

    update controlleur function name

--- assets/changeLike.js
+++ assets/changeLike.js
@@ -3,11 +3,15 @@ document.getElementById('likeLogoId').addEventListener('click', changeLike);
 function changeLike() {
     if (document.getElementById('likeLogoId').classList.contains('like')) {
         document.getElementById('likeLogoId').classList.remove('like');
+        document.getElementById('likeLogoId').classList.remove('fa-thumbs-up');
         document.getElementById('likeLogoId').classList.add('dislike');
+        document.getElementById('likeLogoId').classList.add('fa-thumbs-down');
     }
     else {
         document.getElementById('likeLogoId').classList.remove('dislike');
+        document.getElementById('likeLogoId').classList.remove('fa-thumbs-down');
         document.getElementById('likeLogoId').classList.add('like');
+        document.getElementById('likeLogoId').classList.add('fa-thumbs-up');
     }
 
     let element = document.getElementsByClassName('hiddenLikeCheck');
