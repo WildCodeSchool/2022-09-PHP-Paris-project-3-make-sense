e1802d01c64937a46fdb27217a150cfa8debac9f
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2023-01-26 18:49:06 +0100

    final

--- src/Controller/DecisionController.php
+++ src/Controller/DecisionController.php
@@ -3,12 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Decision;
-use App\Entity\Opinion;
-use App\Form\OpinionType;
 use App\Form\FirstDecisionType;
 use App\Service\OpinionLike;
 use App\Repository\UserRepository;
-use App\Repository\OpinionRepository;
 use App\Repository\DecisionRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,53 +15,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 
 class DecisionController extends AbstractController
 {
-    #[Route('/decision/{decisionId}/opinions/{opinionState}', name: 'app_opinion')]
-    #[Entity('decision', options: ['mapping' => ['decisionId' => 'id']])]
-    public function giveOpinion(
-        Decision $decision,
-        string $opinionState,
-        DecisionRepository $decisionRepository,
-        OpinionLike $opinionLike,
-        UserRepository $userRepository,
-        OpinionRepository $opinionRepository,
-        Request $request
-    ): Response {
-
-        $opinion = $opinionRepository->findOneBy(['user' => HomeController::USERID, 'decision' => $decision->getId()]);
-
-        if (!$opinion) {
-            $opinion = new Opinion();
-            $opinion->setDecision($decision);
-            $opinion->setUser($userRepository->findOneBy(['id' => HomeController::USERID]));
-            if ($opinionState == "like") {
-                $opinion->setIsLike(true);
-            } else {
-                $opinion->setIsLike(false);
-            }
-        }
-
-        $form = $this->createForm(OpinionType::class, $opinion);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $opinionRepository->save($opinion, true);
-
-            return $this->redirectToRoute('app_home');
-        }
-
-        return $this->renderForm(
-            'opinion/index.html.twig',
-            [
-                'form' => $form,
-                'decision' => $decisionRepository->findOneBy(['id' => $decision->getId()]),
-                'opinion' => $opinion,
-                'opinionLike' => $opinionLike->calculateOpinion($decision),
-                'user' => $userRepository->findOneBy(['id' => $decision->getOwner()])
-            ]
-        );
-    }
-
     #[Route('/decision/{decisionId}/firstdecision', name: 'app_conflict')]
     #[Entity('decision', options: ['mapping' => ['decisionId' => 'id']])]
     public function firtDecision(
