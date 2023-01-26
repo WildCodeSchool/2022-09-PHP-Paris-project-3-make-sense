bd688b0beccbe680b9d4bac78a74bb3d759c753a
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2023-01-26 21:51:40 +0100

    new service update history

---
+++ src/Service/UpdateHistory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Decision;
+use App\Entity\History;
+use App\Repository\HistoryRepository;
+
+class UpdateHistory
+{
+    private HistoryRepository $historyRepository;
+
+    public function __construct(HistoryRepository $historyRepository)
+    {
+        $this->historyRepository = $historyRepository;
+    }
+
+    public function updateHistory(Decision $decision, string $status): void
+    {
+        $history = new History();
+        $history->setStatus($status);
+        $history->setStartedAt($decision->getCreatedAt());
+        $history->setEndedAt($decision->getEndAt());
+        $history->setDecision($decision);
+
+        $this->historyRepository->save($history, true);
+    }
+}
