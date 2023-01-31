b567044ed7713a2e3167ba349ff7398fc4253fd8
Cedric78190 <cedric.nkn@gmail.com>
2023-01-31 15:15:14 +0100

    ok_for_pr

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,19 +10,18 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('/user', name: 'index')]
 class UserController extends AbstractController
 {
     #[Route('/{user}', methods: ['GET'], name: 'show')]
-    public function show(User $user, DecisionRepository $decisionRepository, OpinionLike $opinionLike, PaginatorInterface $paginator): Response
+    public function show(User $user, DecisionRepository $decisionRepository, OpinionLike $opinionLike): Response
     {
 
-        $decisions = $decisionRepository->findBy(
-            ['owner' => $user],
-            ['createdAt' => 'DESC']);
+        $decisions = $decisionRepository->findBy(['owner' => $user], ['createdAt' => 'DESC']);
 
         $opinionLikes = $opinionLike->calculateAllOpinion($decisions);
-        
-        return $this->render('user/show.html.twig', ['user' => $user, 'decisions'=>$decisions, 'opinionLikes'=>$opinionLikes]);
+        return $this->render('user/show.html.twig', ['user' => $user, 'decisions' => $decisions,
+        'opinionLikes' => $opinionLikes]);
     }
 }
