ea8b520a63d099b6212c1808ea731bbf6d1811da
Naailah JUGON <jugonnaailah@gmail.com>
2023-02-07 10:35:51 +0100

    update

--- src/Form/DecisionType.php
+++ src/Form/DecisionType.php
@@ -39,14 +39,6 @@ class DecisionType extends AbstractType
                 'label' => 'Titre',
                 'label_attr' => [
                     'class' => 'form-label h4 d-flex justify-content-start mb-3 mt-3']])
-            // ->add('departments', ChoiceType::class, [
-            //     'required' => true,
-            //     'attr' => [
-            //         'class' => 'form-check'],
-            //     'choices' => $choiceDepartments,
-            //     'mapped' => false,
-            //     'expanded' => true,
-            //     'multiple' => true,])
             ->add('departments', EntityType::class, [
                 'class' => Department::class,
                 'required' => true,
